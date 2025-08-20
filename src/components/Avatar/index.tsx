ee151c956a098710157b455b945087b862efb27b
Felipe Luz <felipetluz@outlook.com>
2025-08-20 17:59:13 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { ImgPerfil } from './styles'
+
+const Avatar = () => <ImgPerfil src="https://github.com/devFelipeLuz.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/devFelipeLuz.png" />
-    <Titulo fontSize={20}>Felipe Luz</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Felipe Luz</Titulo>
+      <Paragrafo tipo="secundary" fontSize={16}>
+        devFelipeLuz
+      </Paragrafo>
+      <Descricao tipo="primary" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
